974f03952bd09e9fc7fa4b577d13974f5e31098b
Lloyd Powell <34787887+llmaze3@users.noreply.github.com>
2019-07-01 12:06:34 -0500

    added Files

---
+++ AttackTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour
+{
+    public Collider2D girlWarrior;
+    //Reference is the Collider other 
+    public Canvas can;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("goblin"))
+        {
+            //like clicking the active checkbox. 
+            other.gameObject.SetActive(false);
+            //SetCountText();
+            showGirl();
+        }
+
+    }
+    void showGirl(){
+        girlWarrior.gameObject.SetActive(true);
+        can.gameObject.SetActive(true);
+    }
+
+}
---
+++ SoldierControllerScripts.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierControllerScripts : MonoBehaviour
+{
+
+    public float maxspeed = 10f;
+    bool facingRight = true;
+
+    //Reference to animator
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", Mathf.Abs(move));
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxspeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }          
+    
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
